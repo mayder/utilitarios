95b1f71891a81ab63a3359973f8486e0190469b5
mayder <breno@hamy.com.br>
2019-10-14 15:33:14 -0300

    Correção Hello World

---
+++ src/HelloWorld.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace nagestao\utilitarios;
+
+/**
+ * Description of HelloWorld
+ *
+ * @author breno.mayder
+ */
+class HelloWorld {
+
+    public static function HelloWorld()
+    {
+        return 'Hello World';
+    }
+
+}
