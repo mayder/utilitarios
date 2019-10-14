ff385e01e28dde34fb47f9f561ad9a53316fb5c1
mayder <breno@hamy.com.br>
2019-10-14 14:53:13 -0300

    Utils

---
+++ Utils.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Utils;
+
+/**
+ * Description of Utils
+ *
+ * @author breno.mayder
+ */
+class Utils {
+
+    public function HelloWorld()
+    {
+        return 'Hello World';
+    }
+
+}
