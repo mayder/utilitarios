2eea0782aa882a5f7fb52599b7b46851c9c5bf8b
mayder <breno@hamy.com.br>
2019-11-01 10:45:30 -0300

    Update UtilsMask.php

--- src/UtilsMask.php
+++ src/UtilsMask.php
@@ -45,4 +45,34 @@ class UtilsMask {
         }
     }
     
+    static function limpaPalavraChave($string) {
+        $what = array('ä', 'ã', 'à', 'á', 'â', 'ê', 'ë', 'è', 'é', 'ï', 'ì', 'í', 'ö', 'õ', 'ò', 'ó', 'ô', 'ü', 'ù', 'ú', 'û', 'À', 'Á', 'É', 'Í', 'Ó', 'Ú', 'ñ', 'Ñ', 'ç', 'Ç', ' ');
+        $by = array('a', 'a', 'a', 'a', 'a', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'a', 'a', 'e', 'i', 'o', 'u', 'n', 'n', 'c', 'c', '-');
+        $string = str_replace($what, $by, $string);
+        $string = strtolower($string);
+        return $string;
+    }
+    
+    static function mascaraCPF($valor)
+    {
+        $valor = preg_replace("/\D/", '', $valor);
+        return preg_replace("/(\d{3})(\d{3})(\d{3})(\d{2})/", "\$1.\$2.\$3-\$4", $valor);
+    }
+    
+    static function mascaraCNPJ($valor)
+    {
+        $valor = preg_replace("/\D/", '', $valor);
+        return preg_replace("/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/", "\$1.\$2.\$3/\$4-\$5", $valor);
+    }
+    
+    static function mascaraTelefone($valor){
+        $formatedPhone = preg_replace('/[^0-9]/', '', $valor);
+        $matches = [];
+        preg_match('/^([0-9]{2})([0-9]{4,5})([0-9]{4})$/', $formatedPhone, $matches);
+        if ($matches) {
+            return '('.$matches[1].') '.$matches[2].'-'.$matches[3];
+        }
+        return $valor;
+    }
+    
 }
